43babaaed97df0ef6767b6b20b6699d33a320de1
Yashodeep <yashodeepdeshpande84639@gmail.com>
2024-10-05 15:59:07 +0530

    change the text of button to submit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -51,7 +51,7 @@
     <form action="/login" method="POST">
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
+        <button type="submit">submit</button>
     </form>
 </div>
 </body>
